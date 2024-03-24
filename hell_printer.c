6f6a1488cd5bd36126d35d3ebf2c922f7198085f
Siverslayer <164073129+Siverslayer@users.noreply.github.com>
2024-03-24 15:56:27 +0300

    Create hell_printer.c

---
+++ hell_printer.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int count = 1;
+    while (1) {
+        printf("Go to HELL\n");
+        if (count == 1) {
+            printf("Go to HELL\n");
+        } else if (count == 2) {
+            printf("Go to HELL\n");
+        }
+        count++;
+    }
+    return 0;
+}
